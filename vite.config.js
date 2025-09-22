3eb4f8bbec63c59121435f34bb78d573b7446ba6
Luan.ujk <luanujk@hotmail.com>
2025-09-22 17:06:18 -0300

    cleaning the repository

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Configuração básica para funcionar na Vercel
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // garante paths corretos
-})
