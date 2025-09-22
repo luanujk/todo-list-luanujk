d36dbb2ce798c8dc19e03add0fec503e54d6ff71
Luan.ujk <luanujk@hotmail.com>
2025-09-22 17:17:06 -0300

    here we go again

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- isso é essencial para deploy em Vercel
+  base: './', // garante que os assets sejam relativos
 })
